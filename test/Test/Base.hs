5bc250d0682ab3e5ba21dfe1fe92ad45fa4e6778
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-12-11 15:25:23 +1100

    update test suite

---
+++ test/Test/Base.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE RankNTypes #-}
+-- |
+-- Module      : Test.Base
+-- Copyright   : [2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Test.Base
+  where
+
+import Data.Array.Accelerate                                        ( Z(..), (:.)(..), DIM1, DIM2, DIM3, Shape, Elt, Acc, Array )
+import Data.Array.Accelerate.Array.Sugar                            ( fromList, size )
+import Data.Array.Accelerate.Trafo                                  ( Afunction, AfunctionR )
+import Data.Array.Accelerate.Data.Complex
+import Data.Array.Accelerate.Math.FFT
+
+import Hedgehog
+import qualified Hedgehog.Gen                                       as Gen
+import qualified Hedgehog.Range                                     as Range
+
+import Prelude                                                      as P
+
+
+type RunN = forall f. Afunction f => f -> AfunctionR f
+
+type Transform sh e = Mode -> Acc (Array sh e) -> Acc (Array sh e)
+
+
+f32 :: Gen Float
+f32 = Gen.realFloat (Range.linearFracFrom 0 (-1) 1)
+
+f64 :: Gen Double
+f64 = Gen.realFloat (Range.linearFracFrom 0 (-1) 1)
+
+complex :: Gen a -> Gen (Complex a)
+complex f = (:+) <$> f <*> f
+
+dim1 :: Gen DIM1
+dim1 = (Z :.) <$> Gen.int (Range.linear 1 1024)
+
+dim2 :: Gen DIM2
+dim2 = do
+  x <- Gen.int (Range.linear 1 128)
+  y <- Gen.int (Range.linear 1 48)
+  return (Z :. y :. x)
+
+dim3 :: Gen DIM3
+dim3 = do
+  x <- Gen.int (Range.linear 1 64)
+  y <- Gen.int (Range.linear 1 32)
+  z <- Gen.int (Range.linear 1 16)
+  return (Z :. z :. y :. x)
+
+array :: (Shape sh, Elt e) => sh -> Gen e -> Gen (Array sh e)
+array sh gen = fromList sh <$> Gen.list (Range.singleton (size sh)) gen
+
--- test/Test/FFT.hs
+++ test/Test/FFT.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE ConstraintKinds     #-}
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE RankNTypes          #-}
@@ -17,21 +18,17 @@
 module Test.FFT ( testFFT )
   where
 
+import Test.Base
 import Test.ShowType
 
-import Data.Array.Accelerate                                        ( Shape, Slice, Elt, Array, Acc, Exp, (:.)(..), DIM2 )
-import Data.Array.Accelerate.Trafo                                  ( Afunction, AfunctionR )
-import Data.Array.Accelerate.Array.Sugar                            ( rank )
+import Data.Array.Accelerate                                        as A hiding ( RealFloat, Eq, reverse )
 import Data.Array.Accelerate.Data.Complex
 import Data.Array.Accelerate.Math.FFT
-import Data.Array.Accelerate.Hedgehog.Similar
+import Data.Array.Accelerate.Test.Similar
 import qualified Data.Array.Accelerate                              as A
-import qualified Data.Array.Accelerate.Hedgehog.Gen.Array           as Gen
-import qualified Data.Array.Accelerate.Hedgehog.Gen.Shape           as Gen
 
 import Hedgehog
 import qualified Hedgehog.Gen                                       as Gen
-import qualified Hedgehog.Range                                     as Range
 
 import Test.Tasty
 import Test.Tasty.Hedgehog
@@ -40,22 +37,49 @@ import Data.Proxy
 import Prelude                                                      as P hiding ( reverse )
 
 
--- type Run  = forall a. Arrays a => Acc a -> a
-type RunN = forall f. Afunction f => f -> AfunctionR f
+testFFT :: RunN -> TestTree
+testFFT runN =
+  testGroup "FFT"
+    [ testFFT' f32 runN
+    , testFFT' f64 runN
+    ]
 
-type Transform sh e = Mode -> Acc (Array sh e) -> Acc (Array sh e)
+testFFT'
+    :: forall e. (FFTElt e, Similar e, RealFloat e, Show (ArgType e))
+    => Gen e
+    -> RunN
+    -> TestTree
+testFFT' e runN =
+  testGroup (showType (Proxy::Proxy e))
+    [ testGroup "DIM1"
+      [ testProperty "homogeneity" $ test_homogeneity runN fft1D dim1 e
+      , testProperty "additivity"  $ test_additivity  runN fft1D dim1 e
+      , testProperty "inverse"     $ test_inverse     runN fft1D dim1 e
+      , testProperty "reverse"     $ test_reverse     runN fft1D dim1 e
+      , testProperty "conjugate"   $ test_conjugate   runN fft1D dim1 e
+      , testProperty "isometry"    $ test_isometry    runN fft1D dim1 e
+      , testProperty "unitarity"   $ test_unitarity   runN fft1D dim1 e
+      ]
+    , testGroup "DIM2"
+      [ testProperty "homogeneity" $ test_homogeneity runN fft2D dim2 e
+      , testProperty "additivity"  $ test_additivity  runN fft2D dim2 e
+      , testProperty "inverse"     $ test_inverse     runN fft2D dim2 e
+      , testProperty "reverse"     $ test_reverse     runN fft   dim2 e
+      , testProperty "conjugate"   $ test_conjugate   runN fft   dim2 e
+      , testProperty "isometry"    $ test_isometry    runN fft   dim2 e
+      , testProperty "unitarity"   $ test_unitarity   runN fft   dim2 e
+      ]
+    , testGroup "DIM3"
+      [ testProperty "homogeneity" $ test_homogeneity runN fft3D dim3 e
+      , testProperty "additivity"  $ test_additivity  runN fft3D dim3 e
+      , testProperty "inverse"     $ test_inverse     runN fft3D dim3 e
+      , testProperty "reverse"     $ test_reverse     runN fft   dim3 e
+      , testProperty "conjugate"   $ test_conjugate   runN fft   dim3 e
+      , testProperty "isometry"    $ test_isometry    runN fft   dim3 e
+      , testProperty "unitarity"   $ test_unitarity   runN fft   dim3 e
+      ]
+    ]
 
-
-floating :: P.RealFloat a => Gen a
-floating = Gen.realFloat (Range.linearFracFrom 0 (-1) 1)
-
-complex :: Gen a -> Gen (Complex a)
-complex f = (:+) <$> f <*> f
-
-shape :: forall sh. (Gen.Shape sh, Shape sh) => Gen sh
-shape = Gen.shape (Range.linear 1 (512 `quot` (2 ^ r)))
-  where
-    r = rank (undefined::sh)
 
 mode :: Gen Mode
 mode = Gen.element [Forward, Reverse, Inverse]
@@ -82,157 +106,133 @@ dotc :: (A.RealFloat e, Shape sh)
      -> Acc (Array sh (Complex e))
 dotc xs ys = A.sum $ A.zipWith (*) xs (A.map conjugate ys)
 
+scalar :: Elt e => e -> Scalar e
+scalar x = fromFunction Z (const x)
+
 
 test_homogeneity
-    :: (FFTElt e, Similar e, RealFloat e, Gen.Shape sh, Shape sh, Eq sh)
-    => Proxy e
-    -> RunN
+    :: (FFTElt e, Similar e, Shape sh, Eq sh)
+    => RunN
     -> Transform sh (Complex e)
+    -> Gen sh
+    -> Gen e
     -> Property
-test_homogeneity _ runN transform
-  = property
-  $ do
-        sign  <- forAll mode
-        sh    <- forAll shape
-        arr   <- forAll (Gen.array sh (complex floating))
-        x     <- A.the . A.unit . A.constant <$> forAll (complex floating)
-        --
-        runN (transform sign . A.map (x *)) arr ~~~ runN (A.map (x *) . transform sign) arr
+test_homogeneity runN transform dim e =
+  property $ do
+    sign  <- forAll mode
+    sh    <- forAll dim
+    arr   <- forAll (array sh (complex e))
+    x     <- forAll (complex e)
+    --
+    let !go1 = runN (\u -> transform sign . A.map (the u *))
+        !go2 = runN (\u -> A.map (the u *) . transform sign)
+    --
+    go1 (scalar x) arr ~~~ go2 (scalar x) arr
 
 test_additivity
-    :: (FFTElt e, Similar e, RealFloat e, Gen.Shape sh, Shape sh, Eq sh)
-    => Proxy e
-    -> RunN
+    :: (FFTElt e, Similar e, Shape sh, Eq sh)
+    => RunN
     -> Transform sh (Complex e)
+    -> Gen sh
+    -> Gen e
     -> Property
-test_additivity _ runN transform
-  = property
-  $ do
-      sign <- forAll mode
-      sh   <- forAll shape
-      xs   <- forAll (Gen.array sh (complex floating))
-      ys   <- forAll (Gen.array sh (complex floating))
-      --
-      runN (\u v -> transform sign (A.zipWith (+) u v)) xs ys ~~~ runN (\u v -> A.zipWith (+) (transform sign u) (transform sign v)) xs ys
+test_additivity runN transform dim e =
+  property $ do
+    sign <- forAll mode
+    sh   <- forAll dim
+    xs   <- forAll (array sh (complex e))
+    ys   <- forAll (array sh (complex e))
+    --
+    let !go1 = runN (\u v -> transform sign (A.zipWith (+) u v))
+        !go2 = runN (\u v -> A.zipWith (+) (transform sign u) (transform sign v))
+    --
+    go1 xs ys ~~~ go2 xs ys
 
 test_inverse
-    :: (FFTElt e, Similar e, RealFloat e, Gen.Shape sh, Shape sh, Eq sh)
-    => Proxy e
-    -> RunN
+    :: (FFTElt e, Similar e, Shape sh, Eq sh)
+    => RunN
     -> Transform sh (Complex e)
+    -> Gen sh
+    -> Gen e
     -> Property
-test_inverse _ runN transform
-  = property
-  $ do
-      sh <- forAll shape
-      xs <- forAll (Gen.array sh (complex floating))
-      --
-      xs ~~~ runN (transform Inverse . transform Forward) xs
+test_inverse runN transform dim e =
+  property $ do
+    sh <- forAll dim
+    xs <- forAll (array sh (complex e))
+    --
+    let !go = runN (transform Inverse . transform Forward)
+    xs ~~~ go xs
 
 test_reverse
-    :: (FFTElt e, Similar e, RealFloat e, Gen.Shape sh, Shape sh, Slice sh, Eq sh)
-    => Proxy e
-    -> RunN
+    :: (FFTElt e, Similar e, Shape sh, Slice sh, Eq sh)
+    => RunN
     -> Transform (sh:.Int) (Complex e)
+    -> Gen (sh:.Int)
+    -> Gen e
     -> Property
-test_reverse _ runN transform
-  = property
-  $ do
-      sign <- forAll mode
-      sh   <- forAll shape
-      xs   <- forAll (Gen.array sh (complex floating))
-      --
-      runN (reverse . transform sign) xs ~~~ runN (transform sign . reverse) xs
+test_reverse runN transform dim e =
+  property $ do
+    sign <- forAll mode
+    sh   <- forAll dim
+    xs   <- forAll (array sh (complex e))
+    --
+    let !go1 = runN (reverse . transform sign)
+        !go2 = runN (transform sign . reverse)
+    --
+    go1 xs ~~~ go2 xs
 
 test_conjugate
-    :: (FFTElt e, Similar e, RealFloat e, Gen.Shape sh, Shape sh, Slice sh, Eq sh)
-    => Proxy e
-    -> RunN
+    :: (FFTElt e, Similar e, Shape sh, Slice sh, Eq sh)
+    => RunN
     -> Transform (sh:.Int) (Complex e)
+    -> Gen (sh:.Int)
+    -> Gen e
     -> Property
-test_conjugate _ runN transform
-  = property
-  $ do
-      sign <- forAll mode
-      sh   <- forAll shape
-      xs   <- forAll (Gen.array sh (complex floating))
-      --
-      runN (A.map conjugate . transform sign) xs ~~~ runN (transform sign . A.map conjugate . reverse) xs
+test_conjugate runN transform dim e =
+  property $ do
+    sign <- forAll mode
+    sh   <- forAll dim
+    xs   <- forAll (array sh (complex e))
+    --
+    let !go1 = runN (A.map conjugate . transform sign)
+        !go2 = runN (transform sign . A.map conjugate . reverse)
+    --
+    go1 xs ~~~ go2 xs
 
 test_isometry
-    :: forall sh e. (FFTElt e, Similar e, RealFloat e, Gen.Shape sh, Shape sh, Slice sh, Eq sh)
-    => Proxy e
-    -> RunN
+    :: forall sh e. (FFTElt e, Similar e, Shape sh, Slice sh, Eq sh)
+    => RunN
     -> Transform (sh:.Int) (Complex e)
+    -> Gen (sh:.Int)
+    -> Gen e
     -> Property
-test_isometry _ runN transform
-  = property
-  $ do
-      sign      <- forAll (Gen.element [Forward, Reverse])
-      sh@(_:.n) <- forAll shape
-      xs        <- forAll (Gen.array sh (complex floating))
-      let n'     = A.the . A.unit . A.constant $ sqrt (fromIntegral n)
-      --
-      runN (norm2 . transform sign) xs ~~~ runN (A.map (n' *) . norm2) xs
+test_isometry runN transform dim e =
+  property $ do
+    sign      <- forAll (Gen.element [Forward, Reverse])
+    sh@(_:.n) <- forAll dim
+    xs        <- forAll (array sh (complex e))
+    --
+    let !go1   = runN (norm2 . transform sign)
+        !go2   = runN (\u -> A.map (the u *) . norm2)
+    --
+    go1 xs ~~~ go2 (scalar (sqrt (P.fromIntegral n))) xs
 
 test_unitarity
-    :: forall sh e. (FFTElt e, Similar e, RealFloat e, Gen.Shape sh, Shape sh, Slice sh, Eq sh)
-    => Proxy e
-    -> RunN
+    :: forall sh e. (FFTElt e, Similar e, RealFloat e, Shape sh, Slice sh, Eq sh)
+    => RunN
     -> Transform (sh:.Int) (Complex e)
+    -> Gen (sh:.Int)
+    -> Gen e
     -> Property
-test_unitarity _ runN transform
-  = property
-  $ do
-      sign      <- forAll (Gen.element [Forward, Reverse])
-      sh@(_:.n) <- forAll shape
-      xs        <- forAll (Gen.array sh (complex floating))
-      ys        <- forAll (Gen.array sh (complex floating))
-      let n'     = A.the . A.unit . A.constant $ fromIntegral n
-      --
-      runN (\u v -> dotc (transform sign u) (transform sign v)) xs ys ~~~ runN (\u v -> A.map (n'*) (dotc u v)) xs ys
-
-
-testFFT :: RunN -> TestTree
-testFFT runN =
-  testGroup "FFT"
-    [ testFFT' (Proxy::Proxy Float)  runN
-    , testFFT' (Proxy::Proxy Double) runN
-    ]
-
-testFFT'
-    :: forall e. (FFTElt e, Similar e, RealFloat e, Show (ArgType e))
-    => Proxy e
-    -> RunN
-    -> TestTree
-testFFT' e runN =
-  testGroup (showType e)
-    [ testGroup "DIM1"
-      [ testProperty "homogeneity" $ test_homogeneity e runN fft1D
-      , testProperty "additivity"  $ test_additivity  e runN fft1D
-      , testProperty "inverse"     $ test_inverse     e runN fft1D
-      , testProperty "reverse"     $ test_reverse     e runN fft1D
-      , testProperty "conjugate"   $ test_conjugate   e runN fft1D
-      , testProperty "isometry"    $ test_isometry    e runN fft1D
-      , testProperty "unitarity"   $ test_unitarity   e runN fft1D
-      ]
-    , testGroup "DIM2"
-      [ testProperty "homogeneity" $ test_homogeneity e runN fft2D
-      , testProperty "additivity"  $ test_additivity  e runN fft2D
-      , testProperty "inverse"     $ test_inverse     e runN fft2D
-      , testProperty "reverse"     $ test_reverse     e runN (fft :: Transform DIM2 (Complex e))
-      , testProperty "conjugate"   $ test_conjugate   e runN (fft :: Transform DIM2 (Complex e))
-      , testProperty "isometry"    $ test_isometry    e runN (fft :: Transform DIM2 (Complex e))
-      , testProperty "unitarity"   $ test_unitarity   e runN (fft :: Transform DIM2 (Complex e))
-      ]
-    -- , testGroup "DIM3"
-    --   [ testProperty "homogeneity" $ test_homogeneity e runN fft3D
-    --   , testProperty "additivity"  $ test_additivity  e runN fft3D
-    --   , testProperty "inverse"     $ test_inverse     e runN fft3D
-    --   , testProperty "reverse"     $ test_reverse     e runN (fft :: Transform DIM3 (Complex e))
-    --   , testProperty "conjugate"   $ test_conjugate   e runN (fft :: Transform DIM3 (Complex e))
-    --   , testProperty "isometry"    $ test_isometry    e runN (fft :: Transform DIM3 (Complex e))
-    --   , testProperty "unitarity"   $ test_unitarity   e runN (fft :: Transform DIM3 (Complex e))
-    --   ]
-    ]
+test_unitarity runN transform dim e =
+  property $ do
+    sign      <- forAll (Gen.element [Forward, Reverse])
+    sh@(_:.n) <- forAll dim
+    xs        <- forAll (array sh (complex e))
+    ys        <- forAll (array sh (complex e))
+    --
+    let !go1   = runN (\u v -> dotc (transform sign u) (transform sign v))
+        !go2   = runN (\m u v -> A.map (the m *) (dotc u v))
+    --
+    go1 xs ys ~~~ go2 (scalar (P.fromIntegral n)) xs ys
 
